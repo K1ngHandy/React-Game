ae78cdb4ce13449317e6b198bb447be6cec2e5d5
K1ngHandy <bolds-slicks0g@icloud.com>
2024-12-21 21:58:51 -0500

    add state variables for health and points

--- React-Game/frontend/components/App.js
+++ React-Game/frontend/components/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Stats from './Stats'
+import Game from './Game'
 
 export default function App() {
+  const [health, setHealth] = useState(100)
+  const updateHealth = (points) => {
+    setHealth(health + points)
+  }
+
   return (
     <div>
-      <h2>Hello, World!</h2>
+      <h3>Stats:</h3><Stats healthPoints={health} />
+      <h3>Game:</h3><Game updateHealth={updateHealth} />
     </div>
   )
 }
